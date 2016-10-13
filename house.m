bff62814c7bcf9b58f391c3bce9c1f696a429b19
vickymmcd <vickymmcd@gmail.com>
2016-10-13 16:59:41 -0400

    Made simulation work

--- house.m
+++ house.m
@@ -8,13 +8,18 @@ function [ h ] = house( )
     h.heatCapacity = h.volume*h.specificHeat;
     h.area = (h.length*h.width)+(h.width*h.height)*2+(h.length*h.height)*2; %m^2
     h.thickness = .5; %m
+    h.sunarea = 10; %m^2
+    h.convection = 100; %W/m^2*K
+    h.conduction = 2; %W/mK
+    h.getTemp = @getTemp;
+    h.getEnergy = @getEnergy;
     
-    function [t] = temp(energy)
-        t = energy./c.heatCapacity;
+    function [t] = getTemp(energy)
+        t = energy./h.heatCapacity;
     end
 
-    function [u] = energy(temp)
-        u = temp.*c.heatCapacity;
+    function [u] = getEnergy(temp)
+        u = temp.*h.heatCapacity;
     end
 end
 
--- solarhouse.m
+++ solarhouse.m
@@ -1,9 +1,16 @@
 function solarhouse()
     h = house();
     s = sun();
-    function theflow = flow()
-        deltaT = ;
-        theflow = (s.getIrradiance*h.sunarea)-(h.convection*h.area*deltaT)...
+    e = environment();
+    function theflow = flow(t, U)
+        deltaT = h.getTemp(U)-e.getTemp(t, 0); %TODO: Fix day of year
+        theflow = (s.getIrradiance(t, 0)*h.sunarea)-(h.convection*h.area*deltaT)...
         -(((h.conduction*h.area)/h.thickness)*deltaT);
     end
+  
+    
+    [t,U] = ode45(@flow, [0, 240], h.getEnergy(290));
+    Temp = h.getTemp(U);
+    plot(t,Temp,'r*-');
+
 end
