1da4abae3f74f902ffef5b2f53b2363f4bdda4d6
TShapinsky <tobiasshapinsky@gmail.com>
2016-10-18 15:04:46 -0400

    added sun elevation angle calculation

--- sun.m
+++ sun.m
@@ -4,16 +4,28 @@ function [ s ] = sun( )
     s.getIrradiance = @getIrradiance;
     s.getSolarAngle = @getSolarAngle;
     s.dni           = load('data/DNI/dni.mat');
+    s.lat           = deg2rad(s.dni.lat);
     s.dni           = s.dni.dni;
     
     function [i] = getIrradiance (t, d)
         i = s.dni(find(s.dni(:,2) == floor(d + floor(t/24)) & s.dni(:,3) == floor(mod(t,24)),1),7);
     end
     
-    function [az,el] = getSolarAngle(t, day)
-        az = 90; %deg
-        el = 90; %deg
+    function [el] = getSolarAngle(t, day)
+        d = getDeclinationAngle(day);
+        h = getHourAngle(t);
+        el = asin(sin(s.lat)*sin(d)+cos(s.lat)*cos(d)*cos(h));
     end
+
+    function [h] = getHourAngle(t)
+        h = t-12*deg2rad(15);
+    end
+
+    function [d] = getDeclinationAngle(day)
+        d = 23.45*(pi/180)*sin(2*pi*(284+day)/(36.25));
+    end
+
+    
 
 end
 
