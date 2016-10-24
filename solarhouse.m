24890f577a4ef27f83d6f1e6730ab013231a1faf
TShapinsky <tobiasshapinsky@gmail.com>
2016-10-24 15:36:50 -0400

    added thermal mass flows

--- solarhouse.m
+++ solarhouse.m
@@ -2,20 +2,31 @@ function solarhouse()
     h = house();
     s = sun();
     e = environment();
-    function theflow = flow(t, U)
-        deltaT = h.getTemp(U)-e.getTemp(t, 0); %TODO: Fix day of year
-        theflow = (s.getIrradiance(t, 0)*h.getExposedArea(s.getSolarAngle(mod(t,24),floor(t/24)),0))...
+    tm = thermalMass(h);
+    function flows = flow(t, U)
+        deltaT = h.getTemp(U(1))-e.getTemp(t, 0); %TODO: Fix day of year
+        deltaTTmH = h.getTemp(U(1)) - tm.getTemp(U(2));
+        convectionTmH = h.convection*tm.area*deltaTTmH;
+        sunFlow = (s.getIrradiance(t, 0)*h.getExposedArea(s.getSolarAngle(mod(t,24),floor(t/24)),0));
+        houseFlow = sunFlow*tm.albedo...
         -(((h.conductionInsulation*h.areaInsulation)/h.thicknessInsulation)*deltaT)...
-        -(((h.conductionGlass*h.areaGlass)/h.thicknessGlass)*deltaT);
+        -(((h.conductionGlass*h.areaGlass)/h.thicknessGlass)*deltaT)...
+        -convectionTmH;
+        tmFlow = sunFlow*(1-tm.albedo)...
+            + convectionTmH;
+        flows = [houseFlow;tmFlow];
     end
   
     
-    [t,U] = ode45(@flow, [0, 24*364], h.getEnergy(290));
+    [t,U] = ode45(@flow, [0, 24*3], [h.getEnergy(290),tm.getEnergy(290)]);
 
     t = t./24;
-    Temp = h.getTemp(U);
+    Temp = [h.getTemp(U(:,1)),tm.getTemp(U(:,2))];
     Temp = Temp - 273;
-    plot(t,Temp,'r*-');
+    plot(t,Temp(:,1),'r*-');
+    hold on;
+    plot(t,Temp(:,2),'b*-');
+    hold off;
     xlabel('Time (days)');
     ylabel('Temperature (C)')
     title('House Temperature Over Time');
--- thermalMass.m
+++ thermalMass.m
@@ -2,13 +2,14 @@ function [t] = thermalMass(h)
 %THERMALMASS information about the thermal mass
 %made of paraffin wax
     t.convection = 1; %change value
-    t.height = 1; %change value
+    t.height = 0.05; %change value
     t.specificheat = 2160*1000; %J/m^3
     t.area = h.length*h.width; 
     t.volume = t.area*t.height;
-    t.heatcapacity = t.volume*t.specificheat; 
+    t.heatCapacity = t.volume*t.specificheat; 
     t.getEnergy = @getEnergy;
     t.getTemp = @getTemp;
+    t.albedo  = 0.04;
     
     function [c] = getTemp(energy)
         c = energy./t.heatCapacity;
