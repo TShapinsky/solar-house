5ad5e89dde0fe127319935d8a589c6b048c5aea4
vickymmcd <vickymmcd@gmail.com>
2016-10-27 15:37:02 -0400

    Made control length settable

--- control.m
+++ control.m
@@ -1,15 +1,20 @@
 function [c] = control()
 %information about the control
+    c.length = nan;
     c.getLength = @getLength;
     
     function[l] = getLength(houseTemp, desiredTemp)
-        if houseTemp > desiredTemp
-            l = 1;
-        else if houseTemp == desiredTemp
-            l = .5;
-        else 
-            l = 0;
+        if isnan(c.length)         
+            if houseTemp > desiredTemp
+                l = 1;
+            else if houseTemp == desiredTemp
+                l = .5;
+            else 
+                l = 0;
+                end
             end
+        else
+            l=c.length;
         end
     end
 
